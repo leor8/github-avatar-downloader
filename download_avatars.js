98922b3e98ca65802dbfad0f236c92c88e56b91f
leor8 <leo442183205@gmail.com>
2018-06-11 23:12:30 +0000

    able to read url from request

--- download_avatars.js
+++ download_avatars.js
@@ -4,17 +4,37 @@ var token = require("./secrets.js");
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb){
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  headers: {
-    'User-Agent': 'request';
-  }
 
-  request(url, function(err, res, body){
-    cb(err, body)
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + token.GITHUB_TOKEN
+    }
+  };
+
+  var contributors = {};
+
+  request(options, function(err, res, body){
+    for(var i = 0; i < JSON.parse(body).length; i++){
+      //console.log(JSON.parse(body)[i]);
+      //contributors = JSON.parse(body)[i];
+      contributors[JSON.parse(body)[i].login] = JSON.parse(body)[i];
+    }
+
+    cb(err, contributors)
   });
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+
+  for(var individuals in result){
+    console.log("The avatar owner's url is: ", result[individuals].avatar_url);
+  }
 });
+
+
+
+
