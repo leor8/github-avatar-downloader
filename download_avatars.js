1e7e26e2207e11a0e82da50428a6f04fd653a320
leor8 <leo442183205@gmail.com>
2018-06-11 21:23:07 +0000

    hiding token

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,20 @@
 var request = require("request");
+var token = require("./secrets.js");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
+
+function getRepoContributors(repoOwner, repoName, cb){
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  headers: {
+    'User-Agent': 'request';
+  }
+
+  request(url, function(err, res, body){
+    cb(err, body)
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
