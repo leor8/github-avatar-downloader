9a782290be51f2d594a41ada7f24f7130c87c90c
leor8 <leo442183205@gmail.com>
2018-06-12 00:49:08 +0000

    using .env file

--- download_avatars.js
+++ download_avatars.js
@@ -12,6 +12,7 @@ var request = require("request");
 var token = require("./secrets.js");
 var fs = require('fs');
 
+
 // A welcome message
 console.log("Welcome to the GitHub Avatar Downloader!");
 
@@ -35,7 +36,7 @@ function getRepoContributors(repoOwner, repoName, cb){
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'token ' + token.GITHUB_TOKEN
+      'Authorization': 'token ' + process.env.GITHUB_TOKEN
     }
   };
 
